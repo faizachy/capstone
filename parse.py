0279b3b7f9915c4c1c85fc200d7b5dfd98967773
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-04-11 11:14:12 -0400

    get geometry import

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env pvpython
 from paraview.simple import *
+import vtk
 
 def convert_value(e):
     try:
@@ -38,6 +39,11 @@ def get_file_data(filename):
     if types!=None:
         res[curproblem][cur]=(types,values)
     return res
+def add_outside_face(face_set, f):
+    if f in face_set:
+        face_set.remove(f)
+    else:
+        face_set.add(f)
 
         
 if __name__=="__main__":
@@ -47,20 +53,38 @@ if __name__=="__main__":
         print("usage: python parse.py <file>")
         exit(1)
     res=get_file_data(argv[1])
-    #print(res)
-
-    pos_type, pos_vals = res["Problem ID: 1"]["Mesh node coordinates for Entire model"]
-
-    # get active view
-    renderView1 = GetActiveViewOrCreate('RenderView')
-    renderView1.ViewSize = [1670, 1091]
-
-    for pos in pos_vals[:300]:
+    print(res["Problem ID: 1"].keys())
     
-        # Lets create a sphere
-        sphere=Sphere(Radius = 0.1, Center = pos[1:])
-        Show(sphere)
+    pos_type, pos_vals = res["Problem ID: 1"]["Mesh node coordinates for Entire model"]
+    print(pos_type)
+    fac_type, fac_vals = res["Problem ID: 1"]["Element mesh node connectivity for Entire model"]
+    print(fac_type)
 
-    # save screenshot
-    Render()
-    SaveScreenshot('greenSphereScreenshot.png', renderView1,ImageResolution=[1670, 1091])
+    vtk_points = vtk.vtkPoints()
+    vtk_faces = vtk.vtkCellArray()
+    for pos in pos_vals:
+        vtk_points.InsertNextPoint(pos[1:])
+    
+    faces = set()
+    for f_val in fac_vals:
+        f = tuple(sorted(f_val[1:]))
+        add_outside_face(faces, (f[0], f[1], f[2]))
+        add_outside_face(faces, (f[0], f[1], f[3]))
+        add_outside_face(faces, (f[0], f[2], f[3]))
+        add_outside_face(faces, (f[1], f[2], f[3]))
+    
+    for face in faces:
+        vtk_face = vtk.vtkTriangle()
+        ids = vtk_face.GetPointIds()
+        for i, node in enumerate(face):
+            ids.SetId(i, node - 1) # because magnet export 1-indexed
+        vtk_faces.InsertNextCell(vtk_face)
+        
+    vtk_data = vtk.vtkPolyData()
+    vtk_data.SetPoints(vtk_points)
+    vtk_data.SetPolys(vtk_faces)
+
+    writer = vtk.vtkXMLPolyDataWriter()
+    writer.SetFileName('model_out.vtp')
+    writer.SetInputData(vtk_data)
+    writer.Write()
