586df0b0cbfe6409e6ad5b3a5be156340c89a567
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-12-05 04:55:19 -0500

    file result caching

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import json
+import shutil
 
 import PySimpleGUI as sg
 
@@ -7,6 +9,36 @@ import magnet
 import calling_paraview
 
 sg.theme('GreenTan')
+
+VTK_OUT = 'output/model_out.vtp'
+
+file_cache = {}
+
+def check_if_file_solved(path):
+    try:
+        with open("file_cache.json", "rt") as f:
+            global file_cache
+            file_cache = json.loads(f.read())
+            # print(file_cache)
+            if path in file_cache:
+                modified = os.path.getmtime(path)
+                _, cached_path, cached_modified = file_cache[path]
+                if modified == cached_modified:
+                    return cached_path
+    except Exception as e:
+        print("caught error reading file_cache", e)
+    return None
+
+def save_file_to_cache(path):
+    try:
+        with open("file_cache.json", "wt") as f:
+            file_num = file_cache[path][0] if path in file_cache else len(file_cache)
+            cached_path = f"data_cache/vtk_saved_{file_num}.vtp"
+            shutil.copy(VTK_OUT, cached_path)
+            file_cache[path] = (file_num, cached_path, os.path.getmtime(path))
+            f.write(json.dumps(file_cache))
+    except Exception as e:
+        print("caught error writing file_cache", e)
 
 def main():
     layout = [
@@ -25,14 +57,26 @@ def main():
         if event == sg.WIN_CLOSED:
             break
         if event == 'Run Solution':
+            if "file" not in values:
+                print("no file specified")
+                continue
+
             field = "E" if values["E"] else "B"
             print(f"visualizing {field} field")
-            print("extracting magnet solutions")
-            magnet.set_filename(values["file"])
-            magnet.run_script()
-            print("magnet extraction done, parsing into paraview")
-            calling_paraview.run_parser()
-            print("paraview parsing done")
+
+            solved_path = check_if_file_solved(values["file"])
+            if solved_path is not None:
+                print("using cached file result")
+                shutil.copy(solved_path, VTK_OUT)
+            else:
+                print("extracting magnet solutions")
+                magnet.set_filename(values["file"])
+                magnet.run_script()
+                print("magnet extraction done, parsing into paraview")
+                calling_paraview.run_parser()
+                print("paraview parsing done, saving file to cache")
+                save_file_to_cache(values["file"])
+
             calling_paraview.run_displayer(field)
             print("display conversion done")
             break
